b28a7caa8125dd1aec26a6ab3d5366bb083353c9
Kirill studenikin <st135522@spbu.ru>
2024-11-02 18:50:46 +0300

    create open file function

--- methods.cpp
+++ methods.cpp
@@ -19,13 +19,12 @@ private:
   };
 
 bool BMP::Memory(int height,int width) {
-    data = new (std::nothrow) Pixel*[height]; // Выделяем массив указателей на строки
-    if (!data) return false; // Проверка на успешное выделение памяти
+    data = new (std::nothrow) Pixel*[height];
+    if (!data) return false;
 
     for (int i = 0; i < height; ++i) {
         data[i] = new (std::nothrow) Pixel[width];
-        if (!data[i]) { // Если выделение памяти не удалось
-            // Освобождаем уже выделенную память
+        if (!data[i]) {
             for (int j = 0; j < i; ++j) {
                 delete[] data[j];
             }
@@ -45,4 +44,39 @@ void BMP::FreeMemory(int height) {
         delete[] data;
         data = nullptr;
     }
+}
+
+BMP::BMP(const std::string &filename) {
+    std::ifstream file(filename, std::ios::binary);
+    if (!file) {
+        throw std::runtime_error("Open file error.");
+    }
+
+    file.read(reinterpret_cast<char *>(&header), sizeof(header));
+    if (header.fileType != 0x4D42) {
+        throw std::runtime_error("File have not a BMP file.");
+    }
+
+    file.read(reinterpret_cast<char *>(&infoHeader), sizeof(infoHeader));
+
+    infoHeader.height = std::abs(infoHeader.height);
+    infoHeader.width = std::abs(infoHeader.width);
+
+    if (infoHeader.width == 0 || infoHeader.height == 0) {
+        throw std::runtime_error("Invalid file size.");
+    }
+    file.seekg(header.offsetData, file.beg);
+
+    Memory(infoHeader.height, infoHeader.width);
+
+    for (int i = 0; i < infoHeader.height; ++i) {
+        for (int j = 0; j < infoHeader.width; ++j) {
+            file.read(reinterpret_cast<char *>(&data[i][j]), sizeof(Pixel));
+            if (!file) {
+                throw std::runtime_error("Error reading BMP file.");
+            }
+        }
+    }
+
+    file.close();
 }
